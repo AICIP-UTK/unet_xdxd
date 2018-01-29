1eb6c42f021ac80a51cbf2968a15684d857174f6
Ramin Nabati <mnabati@vols.utk.edu>
2018-01-29 22:40:18 +0000

    first commit

---
+++ train2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
+
+# Only train the v13 model for the MUL data
+
+echo ">>> CLEAN UP"
+echo rm -rf /data/working
+rm -rf /data/working && mkdir -p /data/working
+
+source activate py35 && for train_path in $@; do
+    echo ">>> PREPROCESSING STEP ---------------------------"
+    echo python v5_im.py preproc_train $train_path
+    python v5_im.py preproc_train $train_path
+    echo python v12_im.py preproc_train $train_path
+    python v12_im.py preproc_train $train_path
+
+    ### v13 --------------
+    # Training for v13 model
+    echo ">>>>>>>>>> v13.py"
+    python v13.py validate $train_path
+    # Parametr optimization for v13 model
+    echo ">>>>>>>>>> v13.py"
+    python v13.py evalfscore $train_path
+
+done
